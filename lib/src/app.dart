da7c4e18fe0c5840f0a5d500e71380ef34fc5606
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-11-16 22:54:15 +0530

    Add home page and necessary dependencies

---
+++ lib/src/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'ui/home_page.dart';
+import 'models/app_state.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Demo App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        ),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
