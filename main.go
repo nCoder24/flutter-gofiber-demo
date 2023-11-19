92e81a7d4349a52b66c7d466ef582bc21a3a0d08
Utsab Bera <nCoder24@users.noreply.github.com>
2023-11-19 22:11:47 +0530

    Add port in env

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 
 func main() {
 	config.Config()
-	dbURI := os.Getenv("MONGODB_URI")
-	db := db.GetDB(db.Connect(dbURI))
 
-	app.CreateApp(db).Listen(":8080")
+	dbURI := os.Getenv("MONGODB_URI")
+	port := ":" + os.Getenv("PORT")
+
+	db := db.GetDB(db.Connect(dbURI))
+	app.CreateApp(db).Listen(port)
 }
